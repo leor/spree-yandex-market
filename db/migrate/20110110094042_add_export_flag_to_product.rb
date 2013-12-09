7a5a12a05007ad08cb1a0bf7f0be88842808aaff
leor <leor.thesweetvoice@gmail.com>
2013-12-10 00:11:34 +0400

    removing old migration (renaming problems)

--- db/migrate/20110110094042_add_export_flag_to_product.rb
+++
@@ -1,9 +0,0 @@
-class AddYandexExportFlagToProduct < ActiveRecord::Migration
-  def self.up
-    add_column :spree_products, :export_to_yandex_market, :boolean, :default=>true, :null=>false
-  end
-
-  def self.down
-    remove_column :spree_products, :export_to_yandex_market
-  end
-end
